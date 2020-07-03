23b23ee5839bbff6a66d52ef8e9e00d97c351b2f
Ishmenev <ishmenevev@mail.ru>
2020-07-04 02:25:09 +0300

    actual project

--- frontend/src/actions/saveGame.js
+++
@@ -1,8 +0,0 @@
-export const SAVE_GAME_PROCESS = 'SAVE_GAME_PROCESS';
-
-export function gameSaver(gameObject) {
-    return {
-        type: SAVE_GAME_PROCESS,
-        payload: gameObject
-    }
-}

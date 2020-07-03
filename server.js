f13e2343edecf0447b51d155e650fca24658d68f
Ishmenev <ishmenevev@mail.ru>
2020-07-04 01:14:49 +0300

    one more

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use("/api/level", level);
 // Статические ассеты в продакшене
 if (process.env.NODE_ENV === "production") {
   // Устанавливаем статическую папку
-  app.use(express.static("frontend/build"));
+  app.use(express.static(path.join(__dirname, 'frontend/build')))
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname,  "frontend", "build", "index.html"));
   });

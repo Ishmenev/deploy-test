28d5a021cdf43b2e5221bb172d1686c1d1034af1
Ishmenev <ishmenevev@mail.ru>
2020-08-06 04:28:39 +0300

    done

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Проект запущен на порту ${PORT}`)
 })
